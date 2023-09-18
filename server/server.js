46e754371c9d6c43ca9edb87e441bd7c3fa4773a
Ross F <rfahan@gmail.com>
2023-09-18 10:45:52 -0600

    refactored server.js for apollo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');   // original
+
+// Import Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+
+//Import typeDefs and resolvers
+const typeDefs = require('./schemas/typeDefs');
+const resolvers = require('./schemas/resolvers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,12 +16,22 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Initialize new Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => req // allows each request to pass through context
+});
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Integrate Apollo Server with Express application as middleware
+server.applyMiddleware({ app });
+
+// app.use(routes); // original
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
