ae265f79e81658fbc8eb32a9e5f1af3a170aa8dc
Ross F <rfahan@gmail.com>
2023-09-18 11:43:14 -0600

    refactored server side auth middleware and added .env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+require('dotenv').config(); // import .env variables
 //const routes = require('./routes');   // original
 
 // Import Apollo Server
